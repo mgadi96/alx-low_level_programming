94f0fec6f80f79736dcdf7076a43ee519886f408
Meryem Gadi <gadimeryem96@gmail.com>
2023-03-23 17:28:26 -0700

    Fix code indent

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,19 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-        int largest;
+	int largest;
 
-        if (a > b)
-        {
-                if (a > c)
-                        largest = a;
-                else
-                        largest = c;
-        }
-        else if (b > c)
-                largest = b;
-        else
-                largest = c;
+	if (a > b)
+	{
+		if (a > c)
+			largest = a;
+		else
+			largest = c;
+	}
+	else if (b > c)
+		largest = b;
+	else
+		largest = c;
 
-        return (largest);
+	return (largest);
 }
