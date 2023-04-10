865266d734cade3f4980bd6fa6f955f20391f6de
Meryem Gadi <gadimeryem96@gmail.com>
2023-04-10 16:03:51 -0700

    argc & argv tasks 3 & 4

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: the arguments count
+ * @argv: the arguments list
+ * Return: 0 or 1 if the number args is different than 2
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Erorr\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
+
